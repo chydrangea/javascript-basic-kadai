5c97fc1d96060dd5082c88e74d3280ff0dfbefc8
chydrangea <168275673+chydrangea@users.noreply.github.com>
2024-06-01 16:27:38 +0900

    kadai_mdn_jsディレクトリを追加

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+//日付の取得
+const date = new Date();
+const [month, day, year] = [
+  date.getMonth() + 1,
+  date.getDate(),
+  date.getFullYear(),
+];
+//コンソールへの出力
+console.log(year + '年' +  month + '月' + day + '日');
